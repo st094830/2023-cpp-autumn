8c142333d02c05b90d89cd7ab7995735fa50ba45
Иван Шутёмов <st094830@student.spbu.ru>
2023-10-10 02:20:56 +0300

    Homework4

---
+++ 2023-10.10-homework-4/Task4/Source4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+int main(int, char**)
+{
+	int n = 0; //блок создания массива
+	std::cin >> n;
+	int** a = (int**)malloc(sizeof(int*) * n);
+
+	for (int i = 0; i < n; ++i)
+	{
+		*(a + i) = (int*)malloc(sizeof(int) * n);
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			int r = 0;
+			std::cin >> r;
+			*(*(a + i) + j) = r;
+		}
+	}
+
+	int maximum = (*(*(a + 0) + 0)); //блок поиска и вывода максимума.
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			maximum = std::max(maximum, (*(*(a + i) + j)));
+		}
+	}
+	std::cout << maximum << std::endl;
+
+	for (int i = 0; i < n; ++i)
+	{
+		free(*(a + i));
+	}
+	free(a);
+
+	return EXIT_SUCCESS;
+}
---
+++ 2023-10.10-homework-4/Task7/Source7.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+int main(int, char**)
+{
+	int n = 0;
+	std::cin >> n;
+	int** a = (int**)malloc(sizeof(int*) * n);
+
+	for (int i = 0; i < n; ++i)
+	{
+		*(a + i) = (int*)malloc(sizeof(int) * n);
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			int r = 0;
+			std::cin >> r;
+			*(*(a + i) + j) = r;
+		}
+	}
+
+	int* numb_of_nonp = (int*)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			if ((*(*(a + i) + j)) <= 0)
+			{
+				++(*(numb_of_nonp + i));
+			}
+		}
+	}
+
+	int** positive = (int**)malloc(sizeof(int*) * n);
+
+	for (int i = 0; i < n; ++i)
+	{
+		*(positive + i) = (int*)malloc(sizeof(int) * (n - (*(numb_of_nonp + i))));
+	}
+
+	int delta = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < sizeof((*(positive + i))); ++j)
+		{
+			if ((*(*(a + i) + j)) > 0)
+			{
+				(*(*(positive + i) + j)) = (*(*(a + i) + j + delta));
+			}
+			else
+			{
+				++delta;
+			}
+		}
+		delta = 0;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < sizeof((*(positive + i))); ++j)
+		{
+			std::cout << (*(*(positive + i) + j)) << " ";
+		}
+		std::cout << std::endl;
+	}
+
+
+	for (int i = 0; i < n; ++i)
+	{
+		free(*(a + i));
+	}
+	free(a);
+
+	free(numb_of_nonp);
+
+	for (int i = 0; i < n; ++i)
+	{
+		free(*(positive + i));
+	}
+	free(positive);
+
+	return EXIT_SUCCESS;
+}
